28bad174418923706016ca99fa254b2681e60f99
kloolly <80373910+kloolly@users.noreply.github.com>
2024-07-28 11:01:59 +0700

    Update Practice_11.sql

--- Practice_11.sql
+++ Practice_11.sql
@@ -2,3 +2,17 @@
 
 -- 1. Add a table level PRIMARY KEY constraint to the EMPLOYEE table using the ID column.
 -- The constraint should be enabled at creation.
+
+-- alter table employee
+--     add constraint pk_employee_dept_id primary key (DEPT_ID);
+-- desc employee
+
+-- 2. Create a PRIMARY KEY constraint on the DEPARTMENT table using the ID column. The
+-- constraint should be enabled at creation.
+
+select * from department
+
+alter table department
+    add constraint pk_departmentt_id primary key (ID);
+
+desc department
